35b6f43dd8d4acce6c9926e742ba2c485fce9cc4
Thiago Silva <trsthiago04@gmail.com>
2025-10-08 20:13:43 -0300

    Primeiro commit da API funcionando
    Spring Boot inicializou corretamente
    O controller foi reconhecido
    E o servidor local está escutando na porta padrão 8080 (http://localhost:8080/)

---
+++ src/main/java/br/com/transparencia/TransparenciaSalarialEsgApplication.java
@@ -0,0 +1,11 @@
+package br.com.transparencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransparenciaSalarialEsgApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TransparenciaSalarialEsgApplication.class, args);
+    }
+}
